c661e86b7e4540cdb258893c8f6746c421fbd340
danjust <daju85@gmail.com>
2018-11-30 16:59:16 +0000

    import of gpu definition

--- src/mlpredict/api.py
+++ src/mlpredict/api.py
@@ -1,8 +1,9 @@
 import json
-from mlpredict.prediction import predict_walltime
 from sklearn.externals import joblib
 import pkg_resources
 import os
+
+from mlpredict.prediction import predict_walltime
 
 
 def new_dnn(input_dimension,input_size):
@@ -21,27 +22,76 @@ def new_dnn(input_dimension,input_size):
     return net
 
 
-def import_default(dnn_name):
+def import_dnn(dnn_obj):
+    """Import dnn. Tries local definition first
+    Returns:
+        net: instance of class dnn"""
+    try:
+        if os.path.isfile(dnn_obj):
+            net = import_dnn_file(dnn_obj)
+        else:
+            net = import_dnn_default(dnn_obj)
+    except:
+        net = {}
+        print('Deep neural network representation could not be found')
+    return net
+
+
+def import_dnn_default(dnn_name):
     """Import dnn from default path
     Returns:
         net: instance of class dnn"""
     dnn_path = pkg_resources.resource_filename(
             'mlpredict', 'dnn_architecture/%s.json'
             %dnn_name)
-    net = import_dnn(dnn_path)
+    net = import_dnn_file(dnn_path)
     return net
 
 
-def import_dnn(path):
-    """Import dnn from path
+def import_dnn_file(dnn_path):
+    """Import dnn from local path
     Returns:
         net: instance of class dnn"""
     net = dnn()
-    with open(path) as json_data:
+    with open(dnn_path) as json_data:
         tmpdict = json.load(json_data)
     net['layers'] = tmpdict['layers']
     net['input'] = tmpdict['input']
     return net
+
+
+def import_gpu(gpu_obj):
+    """Import gpu definition. Tries local definition first
+    Returns:
+        gpu_stats"""
+    try:
+        if os.path.isfile(gpu_obj):
+            gpu_stats = import_gpu_file(gpu_obj)
+        else:
+            gpu_stats = import_gpu_default(gpu_obj)
+    except:
+        gpu_stats = {}
+        print('GPU definition could not be found')
+    return gpu_stats
+
+
+def import_gpu_default(gpu_name):
+    """Import gpu definition from default path
+    Returns:
+        gpu_stats"""
+    gpu_file = pkg_resources.resource_filename(
+            'mlpredict', 'GPUs/%s.json' %gpu_name)
+    gpu_stats = import_gpu_file(gpu_file)
+    return gpu_stats
+
+
+def import_gpu_file(gpu_path):
+    """Import gpu definition from local path
+    Returns:
+        gpu_stats"""
+    with open(gpu_path) as json_data:
+        gpu_stats = json.load(json_data)
+    return gpu_stats
 
 
 class dnn(dict):
@@ -154,7 +204,7 @@ class dnn(dict):
                 scaler_file=''):
         """Predicts execution time of class instance
         Args:
-            gpu_def: json file with GPU definition
+            gpu: can be local json file with GPU definition or
             batchsize: default 1
             saved_model: tensorflow model, by default uses model from
                     all GPUs
@@ -173,13 +223,9 @@ class dnn(dict):
             scaler_file = pkg_resources.resource_filename(
                     'mlpredict', 'model/scaler_Conv_all.save')
 
-        gpu_file = pkg_resources.resource_filename(
-                'mlpredict', 'GPUs/%s.json' %gpu)
+        gpu_stats = import_gpu(gpu)
 
         scaler = joblib.load(scaler_file)
-
-        with open(gpu_file) as json_data:
-            gpu_stats = json.load(json_data)
 
         layer,time = predict_walltime(
                 self, model_file, scaler, batchsize, optimizer,
